89b6e08bc18712d292dcfdc4c64a67390a329f03
Vedant Chawla <chawlav@iu.edu>
2018-06-26 17:33:19 -0400

    changes updateHeading to more accurate name, adds a second field to form, and displays the value of the that field in the list.

--- index.js
+++ index.js
@@ -2,21 +2,46 @@
 
 
 
-const form = document.querySelector('form#flickForm')
-
-const updateHeading = function(ev) {
+const form = document.querySelector('form#athleteForm')
+const form2 =document.querySelector('form#artistForm')
+const athletesList = function(ev) {
   ev.preventDefault()
   const f = ev.target
 
-  const flickName = f.flickName.value
+  const athleteName = f.athleteName.value;
+  const item = document.createElement('li')
+  item.textContent = athleteName
 
-  const list = document.querySelector('#flicks')
-  list.innerHTML += `<li>${flickName}</li>`
+  const list = document.querySelector('#athlete')
+  list.appendChild(item)
+
 
   f.reset()
 }
 
-form.addEventListener('submit', updateHeading)
+
+const artistList = function(ev) {
+  ev.preventDefault()
+  const a = ev.target
+
+  const artistName = a.artistName.value
+
+  const item2 = document.createElement('li')
+
+  item2.textContent = artistName
+  console.log(item2)
+ 
+  const list2 = document.querySelector('#artist')
+  list2.appendChild(item2)
+
+
+  a.reset()
+}
+
+form.addEventListener('submit', athletesList)
+form2.addEventListener('submit', artistList)
+
+
 
 
 
